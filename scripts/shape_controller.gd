e72260a429c0ddccba277a6f4a6096881d7a2cbf
The-Quinnzite-Mushroom <quinn@trusler.net>
2024-10-05 09:08:50 -0700

    stashing away

--- scripts/shape_controller.gd
+++ scripts/shape_controller.gd
@@ -3,6 +3,7 @@ extends Node
 #ima work on this later
 var points = []
 var dots = []
+var attacking = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +12,9 @@ func _ready():
 var dot_scene = preload("res://scenes/shape_drawing/dot.tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	if attacking:
+		$Polygon2D.color[3] += delta/2
 	if Input.is_action_just_pressed("add point"):
 		var size = get_viewport().size
 		var point = get_viewport().get_mouse_position()
@@ -30,11 +34,14 @@ func _process(delta):
 	
 #when the mouse is down add a dot 
 func finish_shape():
+	attacking = true
 	deleate_dots()
 	deleate_line()
+	
 	$Polygon2D.polygon = points
 	#points = []
 	$attack_delay.start()
+	
 	#points = []
 func deleate_line():
 	$Line2D.points = []
@@ -47,22 +54,33 @@ func deleate_dots():
 
 
 func _on_attack_delay_timeout():
+	attacking = false
 	$Polygon2D.polygon = []
-	$Area2D/CollisionPolygon2D.polygon = points
+	if len(points) == 2:
+		print($vaporize/CollisionShape2D.get_shape())
+		
+		#$vaporize/CollisionShape2D.shape.set_points( (0,0),(100,100) )
+	else:
+		$vaporize/CollisionPolygon2D.polygon = points
 	$kill_zone_delay.start()
-
 	points = []
+	$Polygon2D.color[3] = 0
 	#pass # Replace with function body.
 
 
 func _on_kill_zone_delay_timeout():
-	$Area2D/CollisionPolygon2D.polygon = []
-	print($Area2D/CollisionPolygon2D.polygon)
+	$vaporize/CollisionPolygon2D.polygon = []
+	print($vaporize/CollisionPolygon2D.polygon)
 
 
 #func _on_collision_polygon_2d_child_entered_tree(node):
 	
 
 func _on_area_2d_area_entered(area):
+	#if area.name == "vaporize":
+		#do shit
+	#print(area)
+	
+	print(area.name)
 	print("colision with an enemy")
 	#pass # Replace with function body.
