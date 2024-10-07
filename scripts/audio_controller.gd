87e0231b108e5503e92ed4555f928f9f21431ff5
TrustingTroller <zackjiang1@gmail.com>
2024-10-07 08:00:52 -0700

    [Push 4.5]

--- scripts/audio_controller.gd
+++ scripts/audio_controller.gd
@@ -16,9 +16,11 @@ func _process(delta: float) -> void:
 	var enemy_count = enemy_manager.get_child_count()
 	var adjustable_volume = 2 * enemy_count - 100
 	$loop_adjustable.volume_db = clampf(adjustable_volume, -100.0, 10.0)
+	print(str($Menu.volume_db) +" | "+ str($Intro.volume_db) +" | "+ str($Loop.volume_db))
 
 
 func _signal_StartButton_pressed() -> void:
+	$Menu.volume_db = -20.0
 	$Menu.stop()
 	$Intro.play()
 
@@ -29,7 +31,12 @@ func _on_Intro_finished() -> void:
 
 
 func _signal_PlayerOverlay_squeedDied() -> void:
-	$Intro.stop()
-	$Loop.stop()
+	var tween = create_tween()
+	$Menu.play()
+	tween.tween_property($Menu, "volume_db", 0, 2.5)
+	tween.set_parallel()
+	tween.tween_property($Intro, "volume_db", -80, 5)
+	tween.set_parallel()
+	tween.tween_property($Loop, "volume_db", -80, 5)
 	$loop_adjustable.stop()
 	$SqueebDie.play()
