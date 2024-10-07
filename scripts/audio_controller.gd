29adec63ffae293c403c7f368521d7ce8525e14e
TrustingTroller <zackjiang1@gmail.com>
2024-10-07 08:15:38 -0700

    [Push 4.6]

--- scripts/audio_controller.gd
+++ scripts/audio_controller.gd
@@ -5,6 +5,7 @@ extends Node
 @export var ltdb = 0
 @export var dbtl = 0
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Menu.play()
@@ -16,13 +17,14 @@ func _process(delta: float) -> void:
 	var enemy_count = enemy_manager.get_child_count()
 	var adjustable_volume = 2 * enemy_count - 100
 	$loop_adjustable.volume_db = clampf(adjustable_volume, -100.0, 10.0)
-	print(str($Menu.volume_db) +" | "+ str($Intro.volume_db) +" | "+ str($Loop.volume_db))
 
 
 func _signal_StartButton_pressed() -> void:
-	$Menu.volume_db = -20.0
-	$Menu.stop()
+	var tween = create_tween()
+	tween.tween_property($Menu, "volume_db", 15, 2.5)
+	await tween.finished
 	$Intro.play()
+	$Menu.stop()
 
 
 func _on_Intro_finished() -> void:
@@ -31,12 +33,14 @@ func _on_Intro_finished() -> void:
 
 
 func _signal_PlayerOverlay_squeedDied() -> void:
+	$SqueebDie.play()
+	$loop_adjustable.stop()
+	# Fucking Tweens ---------
 	var tween = create_tween()
+	$Menu.volume_db = -20.0
 	$Menu.play()
 	tween.tween_property($Menu, "volume_db", 0, 2.5)
 	tween.set_parallel()
 	tween.tween_property($Intro, "volume_db", -80, 5)
 	tween.set_parallel()
 	tween.tween_property($Loop, "volume_db", -80, 5)
-	$loop_adjustable.stop()
-	$SqueebDie.play()
